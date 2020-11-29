765751ada30af78d39e6c99b19ff030f77e2e345
tatajanaa <37144309+tatajanaa@users.noreply.github.com>
2020-11-29 18:07:04 +0100

    subscribe to changes updated

--- assignment4_part1/script.js
+++ assignment4_part1/script.js
@@ -1,70 +1,103 @@
-import { fromEvent } from "https://dev.jspm.io/rxjs@6/_esm2015";
-import { map, filter } from "https://dev.jspm.io/rxjs@6/_esm2015/operators";
-import { interval } from "http://dev.jspm.io/rxjs@6.5.3/_esm2015/internal/observable/interval.js";
-import { concatMap } from "http://dev.jspm.io/rxjs@6.5.3/_esm2015/operators/index.js";
-import { ajax } from "https://dev.jspm.io/rxjs@6/_esm2015/ajax";
+import { fromEvent } from 'https://dev.jspm.io/rxjs@6/_esm2015';
+import { map, filter } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators';
+import { interval } from "http://dev.jspm.io/rxjs@6.5.3/_esm2015/internal/observable/interval.js" 
+import { concatMap } from "http://dev.jspm.io/rxjs@6.5.3/_esm2015/operators/index.js"  
+import { ajax } from  'https://dev.jspm.io/rxjs@6/_esm2015/ajax';
 
-const poll_url = (url) =>
-  interval(100).pipe(concatMap(() => ajax.getJSON(url)));
+ const poll_url = url => interval(100).pipe(concatMap(() => ajax.getJSON(url)))
 
-const warningsUrl = "http://localhost:8080/warnings";
-const warningsSinceUrl =   "http://localhost:8080/warnings/since/2020-11-29T15:40:31.060Z";
-
-
-const warnings = ajax.getJSON(warningsUrl);
-const warningsSinceTime = ajax.getJSON(warningsSinceUrl);
+const warningsUrl = 'http://localhost:8080/warnings'
+const warningsSinceTimeUrl = 'http://localhost:8080/warnings/since/2020-11-29T16:57:10.690Z'
 
 
-var polledWarnings = interval(100).pipe(concatMap((_) => warnings));
-var polledWarningsSinceTime = interval(100).pipe(concatMap((_) => warningsSinceTime));
+
+const warnings = ajax.getJSON(warningsUrl)
+const warningsSinceTime = ajax.getJSON(warningsSinceTimeUrl)
+
+var polledWarnings = interval(100).pipe(concatMap( _ => warnings))
+
+console.log(new Date().toISOString())
 
 
-function getTime() {
-  var time = new Date().toISOString();
-  console.log(time);
-  return time;
-}
 
-//GET MINIMAL SEVERITY
-function getSeverity() {
-  let input = document.getElementById("severity");
-  return input.value;
-}
-
-function subscribeToWarnings() {
-  const a = polledWarnings.subscribe((result) => {
-    const filteredwarnings = result.warnings.filter(
-      (warning) => warning.severity >= getSeverity()
-    );
-    const warningField = document.getElementById("warningDiv");
-    warningField.textContent = JSON.stringify(filteredwarnings);
-  });
-  const b = polledWarningsSinceTime.subscribe((result) => {
-    const warnimgsSince = result.warnings;
-    const warningField = document.getElementById("changesDiv");
-    warningField.textContent = JSON.stringify(warnimgsSince);
-  });
-
-  return { a, b };
-}
-
-function unSubscribeToWarnings(subscriber) {
-  return subscriber.unsubscribe();
-}
-
-var polledSubscriber = subscribeToWarnings();
-
-//OBSERVE CHECKBOX. IF UNCHECKED - UNSUBSRIBE
-var checked = fromEvent(subscribeChekbox, "change").pipe(
-  map((e) => e.target.checked)
-);
-checked.subscribe((checked) => {
-  unSubscribeToWarnings(polledSubscriber);
-  if (checked) {
-    polledSubscriber = subscribeToWarnings();
-  } else {
-    unSubscribeToWarnings(polledSubscriber);
+  //GET MINIMAL SEVERITY
+  function getSeverity() {
+    let input = document.getElementById('severity');
+    return input.value
+ 
   }
-});
 
-//subscribeToPolledWarnings()
+function subscribeToWarnings(){
+    return polledWarnings.subscribe(
+     (result) => {
+       
+        const filteredwarnings = result.warnings.filter( warning => warning.severity >= getSeverity())
+        const warningField = document.getElementById("warningDiv");
+        warningField.textContent = JSON.stringify(filteredwarnings)
+       
+      });
+      
+  }
+
+ 
+  warningsSinceTime.subscribe(
+    (result)  => {
+      const warningsSinceTime = result.warnings
+      const warningField = document.getElementById("changesDiv");
+      warningField.textContent = JSON.stringify(warningsSinceTime)
+
+    }
+    
+
+  )
+  
+  function unSubscribeToWarnings(subscriber){
+
+    return subscriber.unsubscribe()
+  }
+
+  var polledSubscriber = subscribeToWarnings()
+ 
+//OBSERVE CHECKBOX. IF UNCHECKED - UNSUBSRIBE
+  var checked = fromEvent(subscribeChekbox, 'change').pipe(map( e=> e.target.checked))
+  checked.subscribe(checked => {
+    unSubscribeToWarnings(polledSubscriber)
+    if(checked){
+      console.log('subscribe')
+      polledSubscriber = subscribeToWarnings()
+    }else{
+      console.log('unsubcribe')
+      unSubscribeToWarnings(polledSubscriber)
+    }
+  })
+
+
+
+
+//subsribeToWarningsSinceTime()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+               
+
+
+
+
+
+
+
+
+
