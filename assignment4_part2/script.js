b72c15e6d69e4b05133cdc56710c308ad56d8898
tatajanaa <37144309+tatajanaa@users.noreply.github.com>
2020-11-29 18:31:42 +0100

    filter severity implemented

--- assignment4_part2/script.js
+++ assignment4_part2/script.js
@@ -1,5 +1,10 @@
+import { ajax } from  'https://dev.jspm.io/rxjs@6/_esm2015/ajax';
+
 
 var ws = new WebSocket("ws://localhost:8090/warnings" )
+const warningsSinceUrl =   "http://localhost:8080/warnings/since/2020-11-29T17:00:16.333Z";
+
+const warningsSinceTime = ajax.getJSON(warningsSinceUrl)
 
 
 
@@ -7,7 +12,7 @@ var ws = new WebSocket("ws://localhost:8090/warnings" )
     //UNSUBSCRIBE to warnings
     window.unSubscribe = () => {
         if (ws.OPEN) {
-          const message = JSON.stringify("unsubscribe")
+          const message = JSON.stringify('unsubscribe')
           console.log("unsubscribed")
           ws.send(message)
         }
@@ -32,22 +37,41 @@ var ws = new WebSocket("ws://localhost:8090/warnings" )
       }
 
 function showWarningData(warningsData){
- 
-      const filteredwarnings = warningsData.warnings
+      if(getSeverity()!=0){
+        const filteredwarnings = warningsData.warnings
+        .filter((warning) => warning.severity >= getSeverity() && (warnings=>warnings.prediction!=null))
+     
       const warningField = document.getElementById("warningDiv");
       warningField.textContent = JSON.stringify(filteredwarnings)
-
+      }
+      else {
+        const filteredwarnings = warningsData.warnings.filter(warnings=>warnings.prediction!=null)
+        const warningField = document.getElementById("warningDiv");
+        warningField.textContent = JSON.stringify(filteredwarnings)
+      }
     
 
 }
 
+warningsSinceTime.subscribe(
+  (result)  => {
+    const warningsSinceTime = result.warnings
+    const warningField = document.getElementById("changesDiv");
+    warningField.textContent = JSON.stringify(warningsSinceTime)
+
+  }
+  
+
+)
+  
+
+
 function filterSeverity(){
+ 
     var filterButton = document.getElementById("filterButton")
     filterButton.onclick = function load() {
-  
-     
-            subscribe()
-       
+      
+            subscribe()      
     
     }
     
@@ -81,7 +105,7 @@ function filterSeverity(){
 
   window.onload = subscribe;
   onCheckboxClick()
-  //filterSeverity()
+  filterSeverity()
 
     
 
